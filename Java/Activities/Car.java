d49ebb2f7ee560efe60bc0f7e3311885430a3d3c
Jyo404 <Jyothi.Balipilli@ibm.com>
2024-06-23 11:27:51 +0530

    Add files via upload

--- Java/Activities/Car.java
+++ Java/Activities/Car.java
@@ -1,36 +1,32 @@
-package Activities;
-
 public class Car {
-   
-        //Class Member Variables
-        String color;
-        int make;
-        String transmission;
-        int tyres;
-        int doors;
-        
-        //Constructor
-        Car() {
-        tyres = 4;
-        doors = 4;
-        }
+
+    String color;
+    String transmission;
+    int make;
+    int tryes;
+    int doors;
     
-        //Class Methods
-        public void displayCharacterstics(){
-        System.out.println("Color of the Car: " + color);
-        System.out.println("Make of the Car: " + make);
-        System.out.println("Transmission of the Car: " + transmission);
-        System.out.println("Number of doors on the car: " + doors);
-            System.out.println("Number of tyres on the car: " + tyres);
-        }
-    
-        public void accelerate() {
-        System.out.println("Car is moving forward.");
-        }
-        
-        public void brake() {
+    Car ()
+    {
+     tryes=4;
+     doors=4;
+    }
+    public void displayCharacteristics()
+    {
+        System.out.println("Car transmission :"+ transmission);
+        System.out.println("Car color :"+ color);
+        System.out.println("car make :"+make);
+        System.out.println("No of tyres :"+ tryes);
+        System.out.println("no of doors : "+doors);
+    }
+    public void accelarate()
+    {
+System.out.println("Car is moving forward.");
+    }
+    public void brake()
+    {
         System.out.println("Car has stopped.");
-        }
+
     }
 
-
+}
